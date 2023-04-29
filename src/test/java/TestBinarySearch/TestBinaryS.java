240cb6173274b78d50a4614af551c5c7132f1c3a
Cody Hodge <ckdhodge@gmail.com>
2023-04-28 21:35:47 -0600

    Add more test

--- src/test/java/TestBinarySearch/TestBinaryS.java
+++ src/test/java/TestBinarySearch/TestBinaryS.java
@@ -1,10 +1,9 @@
 package TestBinarySearch;
 import BinarySearch.BinarySearch;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestSearchBinary {
+public class TestBinaryS {
     @Test
     public void testBinSearch() {
         int[] a = {2, 3, 5, 7, 8, 10, 12, 15, 18, 20};
@@ -12,5 +11,6 @@ public class TestSearchBinary {
         assertEquals(bin.BinSearch(a, 20), 9);
         assertEquals(bin.BinSearch(a, 2), 0);
         assertEquals(bin.BinSearch(a, 200), -1);
-    }
+        assertEquals(bin.BinSearch(a, -100), -1);
+   }
 }
