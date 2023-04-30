f858cbe44649256b8efd1a2918ccaad83b5a1980
Cody Hodge <ckdhodge@gmail.com>
2023-04-30 00:27:05 -0600

    Finished merged sort

---
+++ src/test/java/TestMergeSort/TestMerge.java
@@ -0,0 +1,19 @@
+package TestMergeSort;
+
+import MergeSort.SortMerge;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestMerge {
+    @Test
+    public void testSort() {
+        int[] a = {38, 27, 43, 3, 9, 82, 10};
+        SortMerge sort = new SortMerge();
+        int[] result = {3,9,10,27,38,43,82};
+        sort.sort(a, 0, a.length - 1);
+        assertArrayEquals(result, a);
+    }
+}
