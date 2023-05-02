28f8a28b64bac3d752753f243f5588310bfd7e84
Cody Hodge <ckdhodge@gmail.com>
2023-05-01 22:34:17 -0600

    Done Quick sort need to add test and comments

--- src/main/java/QuickSort/Main.java
+++ src/main/java/QuickSort/Main.java
@@ -1,4 +1,12 @@
 package QuickSort;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] array = {10, 7, 8, 9, 1, 5};
+        SortQuick quick = new SortQuick();
+        quick.quickSort(array, 0, array.length - 1);
+        for(int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 }
